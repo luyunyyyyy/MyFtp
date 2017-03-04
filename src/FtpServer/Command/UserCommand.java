f38413511584c30debda1fa52ea9326e10d97a33
luyunyyyyy <516537801@qq.com>
2017-03-04 21:31:10 +0800

    完成了UESR命令

--- src/FtpServer/Command/UserCommand.java
+++ src/FtpServer/Command/UserCommand.java
@@ -1,23 +1,40 @@
 package FtpServer.Command;
 
 import FtpServer.ControllerThread;
+import FtpServer.Data;
+import org.apache.log4j.Logger;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+
+import static FtpServer.FtpResponse.PASSWORD_REQURIED_FOR_331;
+import static FtpServer.FtpResponse.SERVER_CANNOT_ACCEPT_ARGUMENT_501;
 
 /**
  * Created by LYY on 2017/3/1.
  */
 public class UserCommand implements Command{
-
+    private static Logger logger = Logger.getLogger(UserCommand.class);
 
     @Override
     public void excuteCommand(String data, BufferedWriter bufferedWriter, ControllerThread controllerThread) throws IOException {
-        bufferedWriter.write("excute user command");
+
+        logger.info("调用USER");
+        bufferedWriter.write("excute USER command");
         String response = "";
+        if(Data.adminUsers.containsKey(data)||Data.users.containsKey(data)){
+            logger.info("用户名:" + data);
+
+            response = PASSWORD_REQURIED_FOR_331;
+        }
+
+        else{
+            logger.info("用户名不存在");
+            response = SERVER_CANNOT_ACCEPT_ARGUMENT_501;
+        }
+
 
         bufferedWriter.write(response);
-        bufferedWriter.write("\r\n");
         bufferedWriter.flush();
     }
 }
