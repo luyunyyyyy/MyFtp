43579016b4a23a9d17377d1ead5cd612e24233b8
luyunyyyyy <516537801@qq.com>
2017-03-05 14:41:10 +0800

    完成了cwd命令 更改工作文件夹

--- src/FtpServer/Command/CwdCommand.java
+++ src/FtpServer/Command/CwdCommand.java
@@ -7,18 +7,28 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 
+import static FtpServer.FtpResponse.CWD_COMMAND_SUCCESFUL_250;
 import static FtpServer.FtpResponse.DIR_ILLEGAL_555;
+import static FtpServer.FtpResponse.FILE_NOT_FOUND_550;
 
 /**
  * Created by LYY on 2017/3/4.
  */
 public class CwdCommand implements Command {
     private static Logger logger = Logger.getLogger(CwdCommand.class);
+
     @Override
     public void excuteCommand(String data, BufferedWriter bufferedWriter, ControllerThread controllerThread) throws IOException {
-        String nowDir = controllerThread.getNowDir() + data;
+        String nowDir = controllerThread.getNowDir() + File.separator + data;
         logger.info("调用CWD 目录:" + nowDir);
-
+        File cwdFile = new File(nowDir);
+        if (cwdFile.exists() && cwdFile.isDirectory()) {
+            controllerThread.setNowDir(nowDir);
+            bufferedWriter.write(CWD_COMMAND_SUCCESFUL_250);
+        } else {
+            bufferedWriter.write(FILE_NOT_FOUND_550);
+        }
+        bufferedWriter.flush();
     }
 
 }
